4d978be6bc183c2c9e3100cbbb219decb3299216
thumbimigwe <thumbimigwe@gmail.com>
2016-03-28 05:22:31 +0300

    save

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'personal',
     'blog',
+    ''
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
