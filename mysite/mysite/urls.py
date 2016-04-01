47eaea278aa2c2d37bcaf7a8723d349c7392359e
thumbimigwe <thumbimigwe@gmail.com>
2016-04-01 20:28:53 +0300

    remake

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('personal.urls')),
     url(r'^blog/', include('blog.urls')),
